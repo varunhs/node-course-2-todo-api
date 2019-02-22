ec2936bbdf3546d818ea0f3c76e09594a86b9b9d
Varun Suresh <varunsuresh02@bitbucket.org>
2019-02-22 15:12:56 +0530

    Add Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-// section 7 lecture 19 needs to be start
-
+// section 7 lecture 20 needs to be start
+const { ObjectID } = require("mongodb");
 var express = require("express");
 var bodyParser = require("body-parser");
 
@@ -59,6 +59,24 @@ app.get("/todos", (req, res) => {
       res.status(400).send(e);
     }
   );
+});
+
+app.get("/todos/:id/", (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
 });
 
 app.listen(3001, () => {
